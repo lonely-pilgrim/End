5d3dc0b87ae8cec82ef8f49ff8765992c04ecd02
Егор 🦁 <egorprog6@gmail.com>
2024-04-21 23:20:20 +0300

    Добавьте файлы проекта.

---
+++ End.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> increaseVector(vector<int> vec, int n) {
+    for (int& num : vec) {
+        num += n;
+    }
+    return vec;
+}
+
+int main() {
+    vector<int> numbers = { 1, 2, 3, 4, 5 };
+    int increment = 10;
+
+    vector<int> increasedNumbers = increaseVector(numbers, increment);
+
+    cout << "Original vector: ";
+    for (int num : numbers) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    cout << "Increased vector: ";
+    for (int num : increasedNumbers) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
